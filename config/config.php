c55fe6fa9d678c7cbe4f00bae29194ea6d1b2000
Chris Crawford <scottishegg@live.co.uk>
2020-01-08 21:46:06 +0000

    Add the initial config options

--- config/config.php
+++ config/config.php
@@ -1,8 +1,17 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+    /**
+     * This can be set to true if you would like Laravel-Trivia to generate
+     * a session token when it is first used, this helps prevent any
+     * duplicate questions and is valid for 6 hours.
+     */
+    'using_session_token' => false,
 
+    /**
+     * Encoding types can either be urlLegacy, url3986 or base64 are
+     * used to format the return data. By default Laravel-Trivia will
+     * use base64.
+     */
+    'encoding_type' => 'base64'
 ];
